88b6ce4ee19ea4c59e0a1b261af461ad08b54df8
yoo <edel1212@naver.com>
2023-10-03 20:45:59 +0900

    public 파일 불러 오기

--- nextjs-intro/component/NavBar.js
+++ nextjs-intro/component/NavBar.js
@@ -8,6 +8,8 @@ const activeNav = (path) => {
 export default function NavBar() {
   return (
     <nav>
+      {/* 👉 public에 있는 정적 파일을 "/"를 사용해서 바로 접근이 가능함 */}
+      <img src="/vercel.svg"></img>
       <Link href="/" legacyBehavior>
         <a className={activeNav("/")}>Home</a>
       </Link>
@@ -17,13 +19,29 @@ export default function NavBar() {
 
       <style jsx>{`
         nav {
-          background-color: tomato;
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
         }
-        a {
-          text-decoration: none;
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
         }
         .active {
-          color: yellow;
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
         }
       `}</style>
     </nav>
