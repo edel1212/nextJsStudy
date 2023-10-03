3a32686b3906d9c7305abb7b0a88ea165874cd74
yoo <edel1212@naver.com>
2023-10-03 21:36:33 +0900

    use Fetch

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+
+const API_KEY = "a3af7d97effb973e78c5fb1fd7787b13";
+
 export default function Home() {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=ko-KR`
+    )
+      .then((res) => res.json())
+      .then((result) => setMovies(result.results))
+      .catch((e) => console(e));
+  }, []);
   return (
     <>
       <h2>Home!</h2>
+      {!movies && <h4>Loading...</h4>}
+      {movies.map((item) => (
+        <div key={item.id}>{item.original_title}</div>
+      ))}
     </>
   );
 }
