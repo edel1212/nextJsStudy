0fef6c77be2d919d5bc2ca018f826467c1c9c2fb
yoo <edel1212@naver.com>
2023-10-03 13:55:57 +0900

    pre-rendering test

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -1,3 +1,28 @@
-export default function Foo() {
-  return "Hi~";
+import { useState } from "react";
+
+export default function Home() {
+  const [count, setCount] = useState(0);
+  return (
+    <>
+      <h2>Counter {count}</h2>
+      <button
+        onClick={() =>
+          setCount((prev) => {
+            return prev + 1;
+          })
+        }
+      >
+        up
+      </button>
+      <button
+        onClick={() =>
+          setCount((prev) => {
+            return prev - 1;
+          })
+        }
+      >
+        down
+      </button>
+    </>
+  );
 }
