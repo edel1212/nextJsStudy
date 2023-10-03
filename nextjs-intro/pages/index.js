98acc54f504749cfc6b5514cba78fdabe0bd94c0
yoo <edel1212@naver.com>
2023-10-03 21:40:37 +0900

    add Style

--- nextjs-intro/pages/index.js
+++ nextjs-intro/pages/index.js
@@ -19,6 +19,28 @@ export default function Home() {
       {movies.map((item) => (
         <div key={item.id}>{item.original_title}</div>
       ))}
+
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie img {
+          max-width: 100%;
+          border-radius: 12px;
+          transition: transform 0.2s ease-in-out;
+          box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+        }
+        .movie:hover img {
+          transform: scale(1.05) translateY(-10px);
+        }
+        .movie h4 {
+          font-size: 18px;
+          text-align: center;
+        }
+      `}</style>
     </>
   );
 }
