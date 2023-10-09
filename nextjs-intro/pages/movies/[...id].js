30bbdcddb48f925e1d1ea04e0d8625ef16422061
yoo <edel1212@naver.com>
2023-10-09 18:32:22 +0900

    all-catch

--- nextjs-intro/pages/movies/[...id].js
+++ nextjs-intro/pages/movies/[...id].js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 export default function Detail() {
   const router = useRouter();
   console.log("------------------");
-  console.log(router.query.id);
+  console.log(router.query.id); // 👉 [] 형태로 들어옴
 
   const id = router.query.id;
   return <div>{id}</div>;
