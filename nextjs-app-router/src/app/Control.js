d38388c9711cf98a21ca01a7d9155d12f880f217
yoo <edel1212@naver.com>
2023-10-14 19:49:14 +0900

    삭제 기능 구현

--- nextjs-app-router/src/app/Control.js
+++ nextjs-app-router/src/app/Control.js
@@ -1,11 +1,24 @@
 "use client";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 export function Control() {
   const params = useParams();
   const id = params.id;
-  console.log(id);
+
+  const router = useRouter();
+
+  const deleteFunc = () => {
+    const options = {
+      method: "DELETE",
+    };
+    fetch("http://localhost:9999/topics/" + id, options)
+      .then((res) => res.json())
+      .then((result) => {
+        router.push(`/`);
+      });
+  };
+
   return (
     <ul>
       <li>
@@ -17,7 +30,14 @@ export function Control() {
             <Link href={`/update/${id}`}>Update</Link>
           </li>
           <li>
-            <button>Delete</button>
+            <button
+              // 👉  onClick={deleteFunc()}   ❌ 해당 방법으로 사용 시 랜더링 시 삭제 메서드가 실행된다!!
+              onClick={() => {
+                deleteFunc();
+              }}
+            >
+              Delete
+            </button>
           </li>
         </>
       ) : null}
