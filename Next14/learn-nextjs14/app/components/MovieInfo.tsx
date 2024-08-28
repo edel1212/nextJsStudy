121e167207dea250fe7591e240521d33e35382ac
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-08-28 12:24:15 +0900

    Css 적용

--- Next14/learn-nextjs14/app/components/MovieInfo.tsx
+++ Next14/learn-nextjs14/app/components/MovieInfo.tsx
@@ -1,12 +1,11 @@
 import { API_URL } from "../(home)/page";
 
 const getMovie = async (id: string) => {
-    throw  Error("eee");
-    const response = await fetch(`${API_URL}/${id}`);
-    return await response.json();
+  const response = await fetch(`${API_URL}/${id}`);
+  return await response.json();
 };
 
-export default async function MovieInfo({id} : {id:string}){
-    const movie = await getMovie(id);
-    return <h6>{JSON.stringify(movie)}</h6>
+export default async function MovieInfo({ id }: { id: string }) {
+  const movie = await getMovie(id);
+  return <h6>{JSON.stringify(movie)}</h6>;
 }
