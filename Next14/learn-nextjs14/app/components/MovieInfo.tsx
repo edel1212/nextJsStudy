23531677100c6b3fb19ed1b6691b9f8a53304318
jeoungho.yoo <jeoungho.yoo@stans.co.kr>
2024-08-28 13:49:07 +0900

    movie Info CSS 적용

--- Next14/learn-nextjs14/app/components/MovieInfo.tsx
+++ Next14/learn-nextjs14/app/components/MovieInfo.tsx
@@ -1,11 +1,28 @@
 import { API_URL } from "../(home)/page";
+import potato from "../../styles/movie-info.module.css";
 
-const getMovie = async (id: string) => {
+export const getMovie = async (id: string) => {
   const response = await fetch(`${API_URL}/${id}`);
   return await response.json();
 };
 
 export default async function MovieInfo({ id }: { id: string }) {
   const movie = await getMovie(id);
-  return <h6>{JSON.stringify(movie)}</h6>;
+  return (
+    <div className={potato.container}>
+      <img
+        src={movie.poster_path}
+        className={potato.poster}
+        alt={movie.title}
+      />
+      <div className={potato.info}>
+        <h1 className={potato.title}>{movie.title}</h1>
+        <h3>⭐️ {movie.vote_average.toFixed()}</h3>
+        <p>{movie.overview}</p>
+        <a href={movie.homepage} target={"_blank"}>
+          Homepage &rarr;
+        </a>
+      </div>
+    </div>
+  );
 }
--- Next14/learn-nextjs14/app/movies/[id]/page.tsx
+++ Next14/learn-nextjs14/app/movies/[id]/page.tsx
@@ -1,25 +1,30 @@
 import React, { Suspense, useState } from "react";
-import MovieInfo from "../../components/MovieInfo";
+import MovieInfo, { getMovie } from "../../components/MovieInfo";
 import MovieVidoes from "../../components/MovieVideos";
-
-interface Props {
+interface IParams {
   params: { id: string };
-  searchParams: { page: string };
+}
+
+export async function generateMetadata({ params: { id } }: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
 }
 
 /**
  * 👍 해당 매서드에서 await 데이터를 직접 call 하지 않기에
  *    ㄴ> async 선언이 사라졌음 따라서 "use client" 사용또한 가능해짐
  */
-export default function movieDetails({ params, searchParams }: Props) {
+export default function MovieDetailPage({ params: { id } }: IParams) {
   return (
-    <>  
+    <>
       {/* 👍 fallback을 통해 로딩중 보여질 UI 사용 */}
       <Suspense fallback={<h1>영화 정보 로딩중</h1>}>
-        <MovieInfo id={params.id}/>
+        <MovieInfo id={id} />
       </Suspense>
       <Suspense fallback={<h1>영화 영상 로딩중</h1>}>
-      <MovieVidoes id={params.id}/>
+        <MovieVidoes id={id} />
       </Suspense>
     </>
   );
